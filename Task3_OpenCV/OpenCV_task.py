0c3de445df2c95b71fe0be1a7b8e6c953ae15a81
AdviSriv <gotoadvika@gmail.com>
2025-01-05 11:24:37 +0530

    Add files via upload

--- Task3_OpenCV/OpenCV_task.py
+++ Task3_OpenCV/OpenCV_task.py
@@ -7,13 +7,23 @@ while True:
     ret, frame = videoCap.read()
     if not ret:
         break 
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    blur = cv.GaussianBlur(gray, (17,17), 0)
-    cv.imshow('Frame', blur)
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    
+    lower_green = np.array([38, 60, 20])
+    upper_green = np.array([100, 255, 255])
 
-    blank = np.zeros(frame.shape, dtype='uint8')
-    cv.imshow('Contours Drawn', blank)
+    mask = cv.inRange(hsv, lower_green, upper_green)
+    contours,_ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
+    for contour in contours:
+        area = cv.contourArea(contour)
+        if area > 200:  
+            x, y, w, h = cv.boundingRect(contour)
+            cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv.putText(frame, "Ball", (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+
+    cv.imshow('frame', frame)
+   
     if cv.waitKey(15) & 0xFF == ord('q'):
         break
         
