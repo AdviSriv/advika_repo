19b8dfb57244ff1fa2412f073992a30046cba178
AdviSriv <gotoadvika@gmail.com>
2025-01-06 18:11:11 +0530

    Update sort_string.py

--- Task1_programs/sort_string.py
+++ Task1_programs/sort_string.py
@@ -6,8 +6,8 @@ for i in range(0, l):
     list.append(s[i])
 
 for i in range(0, l):
-    for j in range(0, l):
-        if list[i] < list[j]:
+    for j in range(i+1, l):
+        if list[i] > list[j]:
             list[i], list[j] = list[j], list[i]
 j = ""
 
